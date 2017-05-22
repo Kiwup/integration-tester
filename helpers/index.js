46492c4228e01ab917be6320359abbb72371e8b6
Frantz Gauthier <frantz@myentropy.org>
2017-05-22 14:53:36 +0200

    initial commit

---
+++ helpers/index.js
@@ -0,0 +1,41 @@
+'use strict'
+
+const Promise = require('bluebird')
+const { MongoClient } = require('mongodb')
+
+/* DATABASES */
+const STATS_URI = 'mongodb://mongodb-test:27017/stats-test'
+const MAIN_URI = 'mongodb://mongodb-test:27017/reelevant-test'
+
+const getConnexionFactory = (uri) => () => MongoClient.connect(uri, {
+  promiseLibrary: Promise
+}).disposer((db) => db.close())
+
+const clearDatabaseFactory = (uri) => () => Promise.using((getConnexionFactory(uri))(), (db) => {
+  return db.collections().map((col) => col.deleteMany({}))
+})
+
+const getStatsConnexion = getConnexionFactory(STATS_URI)
+const getMainConnexion = getConnexionFactory(MAIN_URI)
+const clearStatsDatabase = clearDatabaseFactory(STATS_URI)
+const clearMainDatabase = clearDatabaseFactory(MAIN_URI)
+
+/* OTHERS */
+function binaryParser (res, callback) {
+  res.setEncoding('binary')
+  res.data = ''
+  res.on('data', function (chunk) {
+    res.data += chunk
+  })
+  res.on('end', function () {
+    callback(null, Buffer.from(res.data, 'binary'))
+  })
+}
+
+module.exports = {
+  getStatsConnexion,
+  getMainConnexion,
+  clearStatsDatabase,
+  clearMainDatabase,
+  binaryParser
+}
---
+++ mocks/index.js
@@ -0,0 +1,3 @@
+'use strict'
+
+console.log('we must mount a volume with `docker-compose` to serve your mocked services from this directory')
